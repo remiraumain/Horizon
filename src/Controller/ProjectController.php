4b62883697603ad8a19b9ba40f1a7213f14c1f70
remiraumain <remiraumain@gmail.com>
2019-04-30 10:53:59 +0200

    Added Project entity and stof_doctrine_extensions

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,6 +15,20 @@ class ProjectController extends AbstractController
     {
         return $this->render('project/index.html.twig', [
             'controller_name' => 'ProjectController',
+        ]);
+    }
+
+    /**
+     * @Route("/project/{slug}", name="project_show")
+     */
+    public function show(Project $project)
+    {
+        $project->setName("test")
+            ->setDescription("test");
+
+        return $this->render('project/show.html.twig', [
+            'controller_name' => 'ProjectController',
+            'project' => $project,
         ]);
     }
 }
---
+++ src/Migrations/Version20190430084221.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190430084221 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE project ADD slug VARCHAR(100) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_2FB3D0EE989D9B62 ON project (slug)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_2FB3D0EE989D9B62 ON project');
+        $this->addSql('ALTER TABLE project DROP slug');
+    }
+}
