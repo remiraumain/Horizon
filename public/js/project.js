41d9cedc6c6ee68249273efb448cbfa135a0aa7d
remiraumain <remiraumain@gmail.com>
2019-05-16 01:53:11 +0200

    Faked the data before having the response from the ajax request, purely visual

--- public/js/project.js
+++ public/js/project.js
@@ -4,6 +4,13 @@ $(document).ready(function() {
 
         var link = $(e.currentTarget);
         link.toggleClass('far').toggleClass('fas');
+        if (link.hasClass('far'))
+        {
+            $('.js-like-project-count').text(Number($('.js-like-project-count').text()) - 1);
+        } else
+        {
+            $('.js-like-project-count').text(Number($('.js-like-project-count').text()) + 1);
+        }
 
         $.ajax({
             method: 'POST',
